37b2ad5ab0a9f42cd15cd87cb82c30917719f740
George Pop <cristi.pop63@yahoo.com>
2022-10-01 13:30:08 +0300

    PCMS_4: Implement a way for the user to interact with the system.

--- src/main/java/com/sda/georgepop/petclinic/Main.java
+++ src/main/java/com/sda/georgepop/petclinic/Main.java
@@ -4,10 +4,39 @@ import com.sda.georgepop.petclinic.controller.VetController;
 import com.sda.georgepop.petclinic.repository.VetRepositoryImpl;
 import com.sda.georgepop.petclinic.service.VetServiceImpl;
 import com.sda.georgepop.petclinic.utils.SessionManager;
+import com.sda.georgepop.petclinic.utils.UserOption;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        SessionManager.getSessionFactory();
         VetController vetController = new VetController(new VetServiceImpl(new VetRepositoryImpl()));
-        vetController.createVet();
+        UserOption userOption;
+
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            UserOption.displayAllOptions();
+            System.out.println("Please select an option:");
+            int numericOption = scanner.nextInt();
+            userOption = UserOption.findByNumericOption(numericOption);
+            switch (userOption) {
+                case ADD_VET:
+                    vetController.createVet();
+                    break;
+                case VIEW_ALL_VET:
+                    System.out.println("Not implemented");
+                    break;
+                case UNKNOWN:
+                    System.err.println("Invalid option selected");
+                    break;
+                case EXIT:
+                    System.out.println("Good Bye");
+                    break;
+            }
+
+        } while (userOption != UserOption.EXIT);
+        SessionManager.shutDown();
     }
 }
